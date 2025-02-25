e96fc2f781d26ba9eea1660f929b0a58d58b6674
Grobik228 <lednikoviperiod@tutamail.com>
2025-02-25 13:26:59 +0300

    main2

--- kod.py
+++ kod.py
@@ -16,6 +16,15 @@ class Birds:
 
     def __del__(self):
         print(self.name,'удален')
+    
+    def flying_range(self, n):
+        if self.flying_ability is False:
+            print(self.name,'не летает')
+        else:
+            print(self.name, (self.size * n),'летает')
 
 
-Birds1 = Birds("Папагай",True , 20)
+birds1 = Birds("Папагай",True , 20)
+birds2 = Birds ("Птичка",False, 20)
+birds1.flying_range(10)
+birds2.flying_range(10)
