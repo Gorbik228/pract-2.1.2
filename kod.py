bfe51b871dc11e903746e36f0e482977b186ff5e
Grobik228 <lednikoviperiod@tutamail.com>
2025-02-25 12:53:55 +0300

    main

--- kod.py
+++ kod.py
@@ -1,4 +1,5 @@
 class Birds:
+    
     def __init__(self, name, flying_ability, size ):
         self.name = name
         self.flying_ability = flying_ability
@@ -6,10 +7,15 @@ class Birds:
 
     def souding(self):
         print(self.name, 'будет петь')
+
     def eating(self):
         print(self.name, 'будет есть')
+
     def sleeping(self):
         print(self.name, 'будет спать')
+
     def __del__(self):
         print(self.name,'удален')
+
+
 Birds1 = Birds("Папагай",True , 20)
